48e066933bcc484d5501d8d9233464b59f9bfdce
yuncham <yuncham2.0@gmail.com>
2025-08-03 22:23:29 -0400

    conecxion a bb

--- knexfile.js
+++ knexfile.js
@@ -1,54 +1,22 @@
-// Update with your config settings.
+require("dotenv").config();
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-require("dotenv").config();
-
 module.exports = {
   development: {
     client: "pg",
-    // connection: {
-    //   host: 'sw2-parcial2.cr6qwyiua5dq.us-east-2.rds.amazonaws.com',
-    //   port: 5432,
-    //   user: 'postgres',
-    //   password: '79052451Aa',
-    //   database: 'blockchain_db',
-    //   ssl: {
-    //     rejectUnauthorized: false
-    //   }
-    // },
-    //   connection: {
-    //   host: 'localhost',
-    //   port: 5434,
-    //   user: 'postgres',
-    //   password: '2405',
-    //   database: 'blockchain_db',
-    //   ssl:false
-    //   // ssl: {
-    //   //   rejectUnauthorized: false
-    //   // }
-    // },
-    connection: {
-      host: 'ep-polished-wind-ac2i62ca-pooler.sa-east-1.aws.neon.tech',
-      port: 5432,
-      user: 'neondb_owner',
-      password: 'npg_6XsBMarzo0cQ',
-      database: 'neondb',
-      ssl: { rejectUnauthorized: false },
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./migrations",
     },
+    // Puedes agregar esto si necesitas ver logs de queries:
+    debug: true,
   },
 
   staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
@@ -59,12 +27,8 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
